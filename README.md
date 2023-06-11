13ca4db982053126e0f30cf7e9f37ad90b1b7969
shimura.atsushi <atsushi.shimura.j.a.n.n.s.f.t.z.n@gmail.com>
2023-06-11 13:21:02 +0900

    edit hogehoge

--- README.md
+++ README.md
@@ -96,7 +96,13 @@ Any tsconfig.json can be used, but if you do, set the environment variable `TS_N
 
 
 
-# AWS CLI 
+# AWS CLI (localstack)
+## create route53 hosted zone
+awslocal route53 create-hosted-zone --name henoheno-prp.com --caller-reference henoheno-pro.com
+
+## change recordset
+awslocal route53 change-resource-record-sets --hosted-zone-id ${hosted-zone-id} --change-batch file://recordset.json
+
 ## create iam role
 
 ``` sh
--- src/functions/hello/handler.ts
+++ src/functions/hello/handler.ts
@@ -1,5 +1,5 @@
 import { formatJSONResponse } from '@libs/api-gateway';
-import { Route53Client, ListResourceRecordSetsCommand, ListResourceRecordSetsRequest, ListResourceRecordSetsResponse, ChangeResourceRecordSetsRequest, ChangeResourceRecordSetsCommand } from "@aws-sdk/client-route-53";
+import { Route53Client, ListResourceRecordSetsCommand, ListResourceRecordSetsCommandInput, ListResourceRecordSetsRequest, ListResourceRecordSetsResponse, ChangeResourceRecordSetsRequest, ChangeResourceRecordSetsCommand } from "@aws-sdk/client-route-53";
 
 
 type EventBridge = {
@@ -18,46 +18,67 @@ const CONFIG = {
   region: 'api-northeast-1'
 }
 const hosted_zone_id = 'Z04289661GOO5YGQ4VK7H';
+// const local_hosted_zone_id = 'DD04XMEUC9EW8IK';
 
 // "/hostedzone/4H03NPQUW4QPA6V"
-const record_set_request_command: ListResourceRecordSetsRequest = {
-  HostedZoneId: hosted_zone_id,
-  StartRecordType: 'A'
+
+const ip_addresses = {
+  first: "10.0.0.1",
+  second: "10.0.0.2"
 }
 
-const change_recordset_command: ChangeResourceRecordSetsRequest = {
+
+const record_set_request_input: ListResourceRecordSetsRequest = {
   HostedZoneId: hosted_zone_id,
-  ChangeBatch: {
-    Comment: "change",
-    Changes: [
-      {
-        Action: "UPSERT",
-        ResourceRecordSet: {
-          Name: "a.henoheno-pro.com",
-          Type: "A",
-          TTL: 100,
-          ResourceRecords: [{
-            Value: '5.5.5.5'
-          }]
-        }
-      }
-    ]
-  }
+  StartRecordName: 'a.henoheno-pro.com',
+  StartRecordType: 'A',
+  MaxItems: 1
 }
+
 
 const hello = async (event: EventBridge) => {
   console.log(event)
 
   const client = new Route53Client(CONFIG);
-//  const command = new ListResourceRecordSetsCommand(record_set_request_command);
-//  const response: ListResourceRecordSetsResponse = await client.send(command);
+  const command = new ListResourceRecordSetsCommand(record_set_request_input);
+  const list_response: ListResourceRecordSetsResponse = await client.send(command);
+  const ip_address = switch_ip_address(list_response.ResourceRecordSets[0].ResourceRecords[0].Value);
+  console.log({ip_address});
+  const change_recordset_command: ChangeResourceRecordSetsRequest = {
+    HostedZoneId: hosted_zone_id,
+    ChangeBatch: {
+      Comment: "change",
+      Changes: [
+        {
+          Action: "UPSERT",
+          ResourceRecordSet: {
+            Name: "a.henoheno-pro.com",
+            Type: "A",
+            TTL: 100,
+            ResourceRecords: [{
+              Value: ip_address,
+            }]
+          }
+        }
+      ]
+    }
+  }
+  console.log(list_response);
   const change_command = new ChangeResourceRecordSetsCommand(change_recordset_command);
-  const response = await client.send(change_command);
-  console.log(response);
+  const change_response = await client.send(change_command);
+  console.log(change_response);
   return formatJSONResponse({
     message: `Hello ${event.name}, welcome to the exciting Serverless world!`,
     event,
   });
 };
+
+const switch_ip_address = ((current: string): string => {
+  console.log({current});
+  if(current === ip_addresses.first) {
+    return ip_addresses.second;
+  }
+  return ip_addresses.first;
+})
 
 export const main = hello;
